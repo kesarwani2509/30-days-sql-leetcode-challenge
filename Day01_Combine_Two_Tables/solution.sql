a5c64f98d464b960d165b4754cb5bd79644cac68
Saurabh Kesarwani <126169600+kesarwani2509@users.noreply.github.com>
2026-03-10 04:47:53 +0530

    Add SQL query to combine two tables
    Implement SQL query to combine data from Person and Address tables.

---
+++ Day01_Combine_Two_Tables/solution.sql
@@ -0,0 +1,11 @@
+-- Day 1: Combine Two Tables
+-- LeetCode Problem #175
+
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
