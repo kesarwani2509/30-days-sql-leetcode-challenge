8e1f3d8ae410f3535b2e8ab3fa05857a9ea06dec
Saurabh Kesarwani <126169600+kesarwani2509@users.noreply.github.com>
2026-03-17 02:28:06 +0530

    Create solution.sql

---
+++ Day08_Rising_Temperature/solution.sql
@@ -0,0 +1,8 @@
+-- Day 8: Rising Temperature
+-- LeetCode Problem #197
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature;
