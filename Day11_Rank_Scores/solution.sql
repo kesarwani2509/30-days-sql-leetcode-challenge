8c472f4b15fc057577c08fc9bfa837ee273143bc
Saurabh Kesarwani <126169600+kesarwani2509@users.noreply.github.com>
2026-03-20 01:14:56 +0530

    Create solution.sql

---
+++ Day11_Rank_Scores/solution.sql
@@ -0,0 +1,11 @@
+-- Day 11: Rank Scores
+-- LeetCode Problem #178
+
+SELECT s1.score,
+       (
+         SELECT COUNT(DISTINCT s2.score)
+         FROM Scores s2
+         WHERE s2.score >= s1.score
+       ) AS rank
+FROM Scores s1
+ORDER BY s1.score DESC;
